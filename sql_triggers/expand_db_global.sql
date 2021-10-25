aabeeb4d695ca3b42fb850d87bde9a181fa18236
mihaicroicu <mihai.croicu@pcr.uu.se>
2021-10-25 17:40:03 +0200

    Update expand_db_global.sql

--- sql_triggers/expand_db_global.sql
+++ sql_triggers/expand_db_global.sql
@@ -1,3 +1,6 @@
+-- Identify in the c table which months have changes in spatial dimensions.
+-- For safety, compute -1,0,+1 months
+
 CREATE TABLE public.temp_months_w_changes AS
 with a AS (
 SELECT month_start as month FROM prod.country UNION
@@ -9,6 +12,11 @@ SELECT month_end as month FROM prod.country
    > 2
 )
 SELECT 1 as month_id UNION SELECT * FROM  b WHERE month>1;
+
+-- Since Priogrid only computes yearly PG->C data, but countries change in cshapes at monthly level
+-- We have to do monthly spatial intersects on PG w/ C.
+-- To make this faster, we look a bboxes first and then intersect.
+-- And only look at this for transition months.
 
 DO $$
 DECLARE months INT;
@@ -44,12 +52,14 @@ intersection AS (
     WHERE a.geom && b.geom
 ),
 cself AS (
-    SELECT 300 as month_id, pg_id, c_id
+    SELECT 1 as month_id, pg_id, c_id
     FROM (
              SELECT *, rank() over (partition by pgm_id order by area desc) as rnk FROM intersection
          ) rnk
     WHERE rnk = 1)
 SELECT * FROM cself';
+
+-- This takes about 3 minutes / month to analyze and there are 72 periods to look into.
 
 for months IN SELECT * FROM temp_months_w_changes ORDER BY month_id
     LOOP
@@ -62,6 +72,15 @@ for months IN SELECT * FROM temp_months_w_changes ORDER BY month_id
 end;
 $$;
 
+-- For each month, look at the nearest intersection (temporally back in time) based on table id
+-- Convert c_id to cm_id and pg_id to pgm_id based on table name
+-- And update.
+-- This takes about 9 seconds / month to run. If it takes longer:
+-- --REINDEX prod.priogrid_month;
+-- --REINDEX prod.country_month;
+-- --VACUUM ANALYZE prod.priogrid_month;
+-- --VACUUM ANALYZE prod.country_month;
+-- and make sure you don't have any locks.
 
 DO $$
 DECLARE months INT;
@@ -92,3 +111,5 @@ FOR months IN SELECT  generate_series(1,860) as s ORDER BY s LOOP
 END LOOP;
 END;
 $$;
+
+-- Cleanup
